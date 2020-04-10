a464c179782550471598cbde3dafe67d449a09ad
David Hambraeus <Ddhambraeus@gmail.com>
2020-04-10 22:11:47 +0200

    Added area and reproduction

--- Polygon.py
+++ Polygon.py
@@ -3,8 +3,10 @@ import numpy as np
 
 class Polygon:
     def __init__(self, xlist, ylist):
-        self.xs = np.array(xlist)
-        self.ys = np.array(ylist)
+        # coordinates are shifted such that the first one is at 0,0
+        self.xs = np.array(xlist) - xlist[0]
+        self.ys = np.array(ylist) - ylist[0]
+        self.calculateArea()
 
     def rotate(self, center, theta):
         # Rotate all points theta (float) radians around center (tuple)
@@ -16,3 +18,17 @@ class Polygon:
         # Translate all points with vector
         self.xs = self.xs + vector[0]
         self.ys = self.ys + vector[1]
+
+    def calculateArea(self):
+        n = len(self.xs)
+        area = 0.0
+        for i in range(n):
+            j = (i + 1) % n
+            area += self.xs[i] * self.ys[j]
+            area -= self.xs[j] * self.ys[i]
+        self.area = abs(area) / 2.0
+
+    def createOffspring(self):
+        newXs = self.xs + np.random.random(len(self.xs))/100
+        newYs = self.ys + np.random.random(len(self.ys))/100
+        return Polygon(newXs, newYs)
