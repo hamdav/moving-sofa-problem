3a0ea6b84f9247874ebe07d42ec885c454c70cec
David Hambraeus <Ddhambraeus@gmail.com>
2020-04-12 12:00:05 +0200

    check if offspring is valid

--- main.py
+++ main.py
@@ -9,14 +9,15 @@ from Polygon import Polygon
 
 plt.style.use('fivethirtyeight')
 
-xs = [0]*25 + list(np.linspace(0,-0.5,25)) + [-0.5]*25 + list(np.linspace(-0.5,0,25))
-ys = list(np.linspace(0,-0.5,25)) + [-0.5]*25 + list(np.linspace(-0.5,0,25)) + [0]*25
+n=3
+xs = [0]*n + list(np.linspace(0,-0.5,n,endpoint=False)) + [-0.5]*n + list(np.linspace(-0.5,0,n, endpoint=False))
+ys = list(np.linspace(0,-1.1,n, endpoint=False)) + [-1.1]*n + list(np.linspace(-1.1,0,n, endpoint=False)) + [0]*n
 original = Polygon(xs, ys)
 
 
 # Generate original population
 population = []
-popSize = 10
+popSize = 50
 for _ in range(popSize):
     population.append(deepcopy(original))
 
@@ -30,7 +31,13 @@ for gen in range(N):
     newPopulation = []
     for p in population:
         newPopulation.append(p)
-        newPopulation.append(p.createOffspring())
+        q = p.createOffspring()
+        # If the new offspring has points out of bounds, don't add it
+        if (any(np.logical_and(q.xs > 0, q.ys < 0)) or any(q.ys > 1) or any(q.xs < -1)):
+            continue
+        else:
+            newPopulation.append(q)
+
     population = newPopulation
 
     # Kill those that fail the test
@@ -54,11 +61,35 @@ for gen in range(N):
     del population[popSize:-1]
 
 
-# View hall of fame stuff
-for p in halloffame:
-    plt.figure()
-    plt.fill(p.xs, p.ys)
-    plt.xlim(-1,1)
-    plt.ylim(-1,1)
+fig = plt.figure()
+ax = fig.add_subplot()
 
+# View hall of fame stuff
+def animate(i):
+    p = halloffame[i]
+    ax.clear()
+    ax.fill(p.xs, p.ys)
+    ax.set_title(f"Generation {i*10}")
+    ax.hlines(0,0,3)
+    ax.hlines(1,-1,3)
+    ax.vlines(0,-3,0)
+    ax.vlines(-1,-3,1)
+
+anim = FuncAnimation(fig, animate, frames=len(halloffame), repeat_delay=1000)
+
+seq = moveSequence(halloffame[-1], [], 0)
+
+fig2 = plt.figure()
+ax2 = fig2.add_subplot()
+
+def animate2(i):
+    ax2.clear()
+    ax2.fill(seq[i][0], seq[i][1])
+    ax2.hlines(0,0,3)
+    ax2.hlines(1,-1,3)
+    ax2.vlines(0,-3,0)
+    ax2.vlines(-1,-3,1)
+
+
+anim2 = FuncAnimation(fig2, animate2, frames=len(seq), repeat_delay=1000)
 plt.show()
