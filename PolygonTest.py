9c582349e543d296089069cdcffd2112a18234fe
David Hambraeus <Ddhambraeus@gmail.com>
2020-04-12 12:00:29 +0200

    Update PolygonTest.py

--- PolygonTest.py
+++ PolygonTest.py
@@ -3,18 +3,30 @@ import matplotlib.pyplot as plt
 
 from Polygon import Polygon
 
-p = Polygon([0, 0.5, 1, 1, 0.5, 0],  [0, 0, 0, 1, 1, 1])
+xs = [0]*25 + list(np.linspace(0,-0.5,25)) + [-0.5]*25 + list(np.linspace(-0.5,0,25))
+ys = list(np.linspace(0,-0.5,25)) + [-0.5]*25 + list(np.linspace(-0.5,0,25)) + [0]*25
+p = Polygon(xs, ys)
 
 fig = plt.figure()
 ax = fig.add_subplot()
 
-ax.fill(p.xs, p.ys)
+#ax.fill(p.xs, p.ys)
 
 p.rotate((1, 0), 0.5)
-ax.fill(p.xs, p.ys)
+#ax.fill(p.xs, p.ys)
 
 p.translate([1, 2])
-ax.fill(p.xs, p.ys)
+#ax.fill(p.xs, p.ys)
+
+plt.ion()
+for i in range(100):
+
+    p = p.createOffspring()
+
+    ax.clear()
+    ax.fill(p.xs, p.ys)
+    plt.show()
+    plt.waitforbuttonpress()
 
 
-plt.show()
+
