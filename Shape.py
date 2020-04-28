2128cd467df18640c654f84cab40cf30421f6e78
David Hambraeus <Ddhambraeus@gmail.com>
2020-04-28 23:12:26 +0200

    Changed Shape class to include more functions
    It now calculates orientation, nodeangles and binding lines on init
    and keeps them in the class

--- Shape.py
+++ Shape.py
@@ -2,17 +2,109 @@ import numpy as np
 
 
 class Node:
-    def __init__(self, pos, radius, orientation):
+    def __init__(self, pos, radius, orientation, ID):
         # X and Y position on center
         self.pos = np.array(pos)
         # Radius
         self.r = radius
         # Orientation (+1 or -1) denotes which way around the boundry goes
         self.o = orientation
+        self.ID = ID
 
 
 class Shape:
     def __init__(self, nodes):
         self.nodes = nodes
+        self.find_node_angles()
+        self.calculateBindingLines()
+        self.calculateOrientation()
+
+    def find_node_angles(self):
+        # Sets self.nodeAngles such that self.nodeAngles[ID] is a tuple
+        # of the angle from the x axis that the point at which the
+        # (incomming, outgoing) line touches the node
+
+        N = len(self.nodes)
+
+        # Initialize nodeAngles as an empty dictionary
+        self.nodeAngles = {}
+
+        for nodeIndex in range(N):
+            node1 = self.nodes[nodeIndex]
+            node2 = self.nodes[(nodeIndex+1) % N]
+
+            # Vector from center of 1 to center of 2
+            vector12 = node2.pos - node1.pos
+            dist12 = np.linalg.norm(vector12)
+
+            # Angle vector makes with pos x axis, \in [-pi, pi]
+            theta = np.arctan2(vector12[1], vector12[0])
+
+            # Calculate angles (from vector12) where the line touches
+            # If orientations are the same:
+            if node1.o * node2.o == 1:
+                alpha = np.arccos((node1.r - node2.r)/dist12)
+                beta = alpha
+            # If orientations are different:
+            elif node1.o * node2.o == -1:
+                alpha = np.arccos((node1.r + node2.r)/dist12)
+                beta = alpha-np.pi
+
+            # If node1s orientation is 1, flip the signs of the angles
+            alpha *= -node1.o
+            beta *= -node1.o
+
+            # Add theta to the angles
+            alpha += theta
+            beta += theta
+
+            # Update nodeAngles
+            if node1.ID in self.nodeAngles:
+                self.nodeAngles[node1.ID][1] = alpha
+            else:
+                self.nodeAngles[node1.ID] = [None, alpha]
+
+            if node2.ID in self.nodeAngles:
+                self.nodeAngles[node2.ID][0] = beta
+            else:
+                self.nodeAngles[node2.ID] = [beta, None]
+
+    def calculateBindingLines(self):
+        self.lines = []
+        N = len(self.nodes)
+        for nodeIndex in range(N):
+            node1 = self.nodes[nodeIndex]
+            node2 = self.nodes[(nodeIndex+1) % N]
+
+            alpha = self.nodeAngles[node1.ID][1]
+            beta = self.nodeAngles[node2.ID][0]
+
+            linePoint1 = node1.pos + node1.r*np.array([np.cos(alpha), np.sin(alpha)])
+            linePoint2 = node2.pos + node2.r*np.array([np.cos(beta), np.sin(beta)])
+
+            self.lines.append(np.array([linePoint1, linePoint2]))
+
+
+    def calculateOrientation(self):
+        # Set self.positive orientation to true if the shape is 
+        # positively oriented, that is, if the total angle change 
+        # by the nodes is positive. 
+        # If the shape is positively oriented, ALL nodes with positive
+        # orientation are inside the shape and ALL nodes with negative
+        # orientation are outside. 
+        cumulative_angle_change = 0
+        for node in self.nodes:
+            # Calculate the angle change due to node
+            angleIn, angleOut = self.nodeAngles[node.ID]
+            angleChange = (angleOut - angleIn) % (2 * np.pi)
+            print(angleChange *180/np.pi)
+            if node.o == 1:
+                cumulative_angle_change += angleChange
+            elif node.o == -1:
+                cumulative_angle_change -= (2*np.pi - angleChange)
+
+        print(cumulative_angle_change * 180/np.pi)
+        self.positive_orientation = cumulative_angle_change > 0
+
 
     #def calculateArea(self):
--- ShapeTests.py
+++ ShapeTests.py
@@ -1,5 +1,7 @@
-from Plot import binding_line
+from PlotShape import binding_line
+from PlotShape import plotShape
 from Shape import Node
+from Shape import Shape
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -13,41 +15,58 @@ def make_artist(node):
     return [circle, arrow]
 
 
-node1 = Node([5, 5], 1, 1)
-node2 = Node([3, 7], 1, 1)
-node3 = Node([5, -5], 1, 1)
-node4 = Node([4, -7], 1, -1)
-node5 = Node([-5, -5], 1, 1)
-node6 = Node([-8, -7], 0.3, -1)
-node7 = Node([-5, 5], 0.3, -1)
-node8 = Node([-2, 7], 1.3, 1)
+def testShapePlotting():
+    nodes = [Node([0,0],1,1,0)]
+    nodes.append(Node([5,1],1,-1,1))
+    nodes.append(Node([4,-6],1,1,2))
+    nodes.append(Node([7,3],1,1,3))
+    s = Shape(nodes)
 
-line1 = binding_line(node1, node2)
-line2 = binding_line(node3, node4)
-line3 = binding_line(node5, node6)
-line4 = binding_line(node7, node8)
+    fig, ax = plt.subplots()
+    ax.set_ylim(-10, 10)
+    ax.set_xlim(-10, 10)
 
-patches = []
-patches.extend(make_artist(node1))
-patches.extend(make_artist(node2))
-patches.extend(make_artist(node3))
-patches.extend(make_artist(node4))
-patches.extend(make_artist(node5))
-patches.extend(make_artist(node6))
-patches.extend(make_artist(node7))
-patches.extend(make_artist(node8))
+    plotShape(s, ax)
+    plt.show()
 
-fig, ax = plt.subplots()
-ax.set_ylim(-10, 10)
-ax.set_xlim(-10, 10)
+testShapePlotting()
+
+def testNodesAndBindingLine():
+    node1 = Node([5, 5], 1, 1, 1)
+    node2 = Node([3, 7], 1, 1, 2)
+    node3 = Node([5, -5], 1, 1, 3)
+    node4 = Node([4, -7], 1, -1, 4)
+    node5 = Node([-5, -5], 1, 1, 5)
+    node6 = Node([-8, -7], 0.3, -1, 6)
+    node7 = Node([-5, 5], 0.3, -1, 7)
+    node8 = Node([-2, 7], 1.3, 1, 8)
+
+    line1 = binding_line(node1, node2)
+    line2 = binding_line(node3, node4)
+    line3 = binding_line(node5, node6)
+    line4 = binding_line(node7, node8)
+
+    patches = []
+    patches.extend(make_artist(node1))
+    patches.extend(make_artist(node2))
+    patches.extend(make_artist(node3))
+    patches.extend(make_artist(node4))
+    patches.extend(make_artist(node5))
+    patches.extend(make_artist(node6))
+    patches.extend(make_artist(node7))
+    patches.extend(make_artist(node8))
+
+    fig, ax = plt.subplots()
+    ax.set_ylim(-10, 10)
+    ax.set_xlim(-10, 10)
 # ax.grid(True)
 
-for patch in patches:
-    ax.add_artist(patch)
+    for patch in patches:
+        ax.add_artist(patch)
 
-ax.plot(line1[:, 0], line1[:, 1])
-ax.plot(line2[:, 0], line2[:, 1])
-ax.plot(line3[:, 0], line3[:, 1])
-ax.plot(line4[:, 0], line4[:, 1])
+    ax.plot(line1[:, 0], line1[:, 1])
+    ax.plot(line2[:, 0], line2[:, 1])
+    ax.plot(line3[:, 0], line3[:, 1])
+    ax.plot(line4[:, 0], line4[:, 1])
 
-plt.show()
+    plt.show()
