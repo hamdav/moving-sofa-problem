c0f44862ee089a8f5510f6851504d507e9f5c01b
David Hambraeus <Ddhambraeus@gmail.com>
2020-04-28 23:05:43 +0200

    Added plotShape function
    plotShape takes a shape argument and an ax argument and plots the shape
    on the axis.

--- PlotShape.py
+++ PlotShape.py
@@ -2,6 +2,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+def make_artist(node):
+    # Creates an artist object from node
+    circle = plt.Circle(node.pos, node.r)
+    arrowbase = node.pos + node.r * np.array([0.5 * node.o, 0.5])
+    arrowdelta = node.r * np.array([-node.o, 0])
+    arrow = plt.Arrow(*arrowbase, *arrowdelta, node.r/2, color="r")
+
+    return [circle, arrow]
+
+
 def binding_line(node1, node2):
     # Return line kissing both nodes on appropriate sides
 
@@ -13,7 +23,7 @@ def binding_line(node1, node2):
     theta = np.arctan2(vector12[1], vector12[0])
 
     # Calculate angles (from vector12) where the line touches
-    # If orientations are the same: 
+    # If orientations are the same:
     if node1.o * node2.o == 1:
         alpha = np.arccos((node1.r - node2.r)/dist12)
         beta = alpha
@@ -30,4 +40,20 @@ def binding_line(node1, node2):
     linePoint2 = node2.pos + node2.r*np.array([np.cos(beta+theta), np.sin(beta+theta)])
 
     return np.array([linePoint1, linePoint2])
+
+
+def plotShape(shape, ax):
+    # Plots shape on axis
+    # Plot all of the nodes:
+    N = len(shape.nodes)
+
+    for node in shape.nodes:
+        nodeArtist = make_artist(node)
+        for artist in nodeArtist:
+            ax.add_artist(artist)
+
+    for line in shape.lines:
+        ax.plot(line[:,0], line[:,1])
+
+
 
