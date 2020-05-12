20048d79ececde261b9c51d23110b55a1499357e
David Hambraeus <Ddhambraeus@gmail.com>
2020-05-12 14:18:24 +0200

    Added segmentIntersectsArc function
    This function checks if a given line segment intersects a given circle
    arc

--- LineMath.py
+++ LineMath.py
@@ -1,16 +1,74 @@
 import numpy as np
 
 
+def rotMat(theta):
+    # Returns a rotation matrix that rotates vectors around (0,0)
+    s = np.sin(theta)
+    c = np.cos(theta)
+    return np.array([[c, -s],
+                     [s, c]])
+
+
 def boxesIntersect(line1, line2):
     # If the smallest box around line 1 intersects the smallest box
     # around line 2, return true, else return false.
     return line1[0][0] <= line2[1][0] and line1[1][0] >= line2[0][0] and line1[0][1] <= line2[1][1] and line1[1][1] >= line2[0][1]
 
 
-def segmentIntersectsArc(line, center, radius, angles):
-    # Returns true if line intersects the arc defined py center
-    # radius and angles, check https://math.stackexchange.com/questions/2744422/line-segment-and-arc-intersection
-    pass
+def segmentIntersectsArc(line, center, radius, angles, orientation):
+    # Returns true if line intersects the arc defined by center,
+    # radius, angles =[start, end] and orientation: +1 or -1
+    # Touching at endpoint is not intersecting, e.g. a line ending at a
+    # point on the circle arc beginning will not be considered intersecting
+
+    # If orientation is -1, switch angleIn and angleOut
+    if orientation == -1:
+        angles = angles[::-1]
+
+    # Translate so that arc center is at 0, 0
+    line = line - center
+
+    # Rotate so that line is horizontal
+    theta = np.arctan2(line[1][1] - line[0][1], line[1]
+                       [0] - line[0][0]) % (2 * np.pi)
+    line = np.array([np.matmul(rotMat(-theta), point) for point in line])
+    lineY = line[0][1]
+    angleBegin = (angles[0] - theta) % (2 * np.pi)
+    angleEnd = (angles[1] - theta) % (2 * np.pi)
+
+    # If the line does not intersect the circle, it definitely doesn't
+    # intersect the arc
+    if abs(lineY) > radius:
+        return False
+
+    # The line intersects the circle at x values positive and negative xIntersection.
+    xIntersection = np.sqrt(radius**2-abs(lineY)**2)
+
+    candidates = []
+    # Decide where the segment intersects the circle
+    if (min(line[0][0], line[1][0]) < xIntersection and
+            max(line[0][0], line[1][0]) > xIntersection):
+        candidates.append(xIntersection)
+    if (min(line[0][0], line[1][0]) < -xIntersection and
+            max(line[0][0], line[1][0]) > -xIntersection):
+        candidates.append(-xIntersection)
+
+    if not candidates:
+        return False
+
+    for x in candidates:
+        phi = np.arctan2(lineY, x) % (2 * np.pi)
+        if angleBegin < angleEnd:
+            # Arc does not go across 0
+            if angleBegin < phi and phi < angleEnd:
+                return True
+        else:
+            # Arc does go across 0 angle, making begin and end angles opposite
+            # Theta is in arc if it is greater than begin or less than end
+            if phi > angleBegin or phi < angleEnd:
+                return True
+
+    return False
 
 
 def linesIntersect(line1, line2):
@@ -25,6 +83,8 @@ def linesIntersect(line1, line2):
     # have different signs
     # p11 is the first point on line1 and so on.
     # All arguments should be numpy arrays.
+
+    # Does the function inlude endpoints? I think so
 
     vector1121 = line1[0] - line2[0]
     vector1122 = line1[0] - line2[1]
