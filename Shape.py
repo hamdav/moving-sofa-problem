21471ef283c5ab79171c407da511e1c256f0bbb4
David Hambraeus <Ddhambraeus@gmail.com>
2020-05-12 14:25:30 +0200

    Structured handeling of angles and pep8 edits
    angles should be 2*pi

--- Shape.py
+++ Shape.py
@@ -19,16 +19,17 @@ class Shape:
 
     # self.nodes: list of node objects
 
-    # self.nodeAngles: dictionare with node.ID as keys and 
-    # [angleIn, angleOut] as values. The angles are the angles at which
+    # self.nodeAngles: dictionare with node.ID as keys and
+    # np.array([angleIn, angleOut]) as values. The angles are the angles at which
     # the incomming and outgoing lines touch the node,
     # counted positively and from the x axis.
+    # Angles are always between 0 and 2 pi.
 
     # self.o: 1 if orientation is positive, -1 if it is negative
     # orientation is calculated from node angles, if the shape turns
     # more in the positive direction, its positive and vice versa.
 
-    # self.lines: list of binding lines on the form 
+    # self.lines: list of binding lines on the form
     # np.array([[[x1,y1], [x2, y2]], [[x1, y1], [x2, y2]], ...])
 
     # self.area: the area of the shape
@@ -79,16 +80,20 @@ class Shape:
             alpha += theta
             beta += theta
 
+            # Make angles be between 0 and 2 pi
+            alpha = alpha % (2*np.pi)
+            beta = beta % (2*np.pi)
+
             # Update nodeAngles
             if node1.ID in self.nodeAngles:
                 self.nodeAngles[node1.ID][1] = alpha
             else:
-                self.nodeAngles[node1.ID] = [None, alpha]
+                self.nodeAngles[node1.ID] = np.array([None, alpha])
 
             if node2.ID in self.nodeAngles:
                 self.nodeAngles[node2.ID][0] = beta
             else:
-                self.nodeAngles[node2.ID] = [beta, None]
+                self.nodeAngles[node2.ID] = np.array([beta, None])
 
     def calculateBindingLines(self):
         self.lines = []
@@ -100,27 +105,31 @@ class Shape:
             alpha = self.nodeAngles[node1.ID][1]
             beta = self.nodeAngles[node2.ID][0]
 
-            linePoint1 = node1.pos + node1.r*np.array([np.cos(alpha), np.sin(alpha)])
-            linePoint2 = node2.pos + node2.r*np.array([np.cos(beta), np.sin(beta)])
+            linePoint1 = node1.pos + node1.r * \
+                np.array([np.cos(alpha), np.sin(alpha)])
+            linePoint2 = node2.pos + node2.r * \
+                np.array([np.cos(beta), np.sin(beta)])
 
             self.lines.append(np.array([linePoint1, linePoint2]))
 
     def calculateOrientation(self):
-        # Set self.positive orientation to true if the shape is 
-        # positively oriented, that is, if the total angle change 
-        # by the nodes is positive. 
+        # Set self.positive orientation to true if the shape is
+        # positively oriented, that is, if the total angle change
+        # by the nodes is positive.
         # If the shape is positively oriented, ALL nodes with positive
         # orientation are inside the shape and ALL nodes with negative
-        # orientation are outside. 
+        # orientation are outside.
         cumulativeAngleChange = 0
         for node in self.nodes:
             # Calculate the angle change due to node
             angleIn, angleOut = self.nodeAngles[node.ID]
-            angleChange = (angleOut - angleIn) % (2 * np.pi)
+            # Angle change is out - in if positively oriented,
+            # and in - out if negatively oriented
+            angleChange = (node.o*(angleOut - angleIn)) % (2 * np.pi)
             if node.o == 1:
                 cumulativeAngleChange += angleChange
             elif node.o == -1:
-                cumulativeAngleChange -= (2*np.pi - angleChange)
+                cumulativeAngleChange -= angleChange
 
         self.o = 1 if cumulativeAngleChange > 0 else -1
 
@@ -146,7 +155,7 @@ class Shape:
 
             # Calculate and add the circle sector area
             # The angle change is out - in if positively oriented
-            # but in - out if negatively oriented. 
+            # but in - out if negatively oriented.
             deltaAngle = (node.o * (angleOut - angleIn)) % (2*np.pi)
             # Circle sector should be added if inside and subtracted if
             # outside
