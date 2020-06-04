e2608a2c2926b2490cb7a9ab2f9fc1ba42a8f02f
David Hambraeus <Ddhambraeus@gmail.com>
2020-06-04 15:21:42 +0200

    Shape overhaul 2: privatizing and random
    Added method for initializing random Shape.
    Also made a bunch of methods and variables private and properties

--- Shape.py
+++ Shape.py
@@ -17,6 +17,26 @@ class Node:
         self.o = orientation
         self.ID = ID
 
+    @staticmethod
+    def randomNode(xlim=(-0.5, 0.5), ylim=(-0.5, 0.5), o=None):
+        # Generate orientation
+        if o is None:
+            o = np.random.randint(1) * 2 - 1
+
+        # Generate positions within x- and ylim
+        sizes = np.array([xlim[1] - xlim[0], ylim[1] - ylim[0]])
+        pos = np.random.random_sample(size=2) * sizes + [xlim[1], ylim[0]]
+
+        # Generate radius such that constraints are upheld,
+        # but not large than one half of the interval
+        maximumRboundry = min(np.abs([pos[0] - xlim[0], pos[0] - xlim[1],
+                              pos[1] - ylim[0], pos[1] - ylim[1]]))
+        maximumRfraction = min(xlim[1] - xlim[0], ylim[1] - ylim[0])/2
+        maximumR = min(maximumRboundry, maximumRfraction)
+        r = np.random.random_sample() * maximumR
+
+        return Node(pos, r, o)
+
 
 class Shape:
     """
@@ -43,11 +63,38 @@ class Shape:
     self.area: the area of the shape
     """
 
-    def __init__(self, nodes):
-        self.nodes = nodes
-        self.recalculate()
+    def __init__(self, nodes=None, xlim=[-0.5, 0.5], ylim=[-0.5, 0.5]):
+        if nodes is None:
+            self.__setNodesRandom(xlim, ylim)
+        else:
+            self._nodes = nodes
+            self.__recalculate()
 
-    def recalculate(self):
+    @property
+    def valid(self):
+        return self._valid
+
+    @property
+    def nodes(self):
+        return self._nodes.copy()
+
+    @property
+    def area(self):
+        return self._area
+
+    @property
+    def nodeAngles(self):
+        return self._nodeAngles.copy()
+
+    @property
+    def lines(self):
+        return self._lines.copy()
+
+    @property
+    def o(self):
+        return self._o
+
+    def __recalculate(self):
         """"
         Recalculates nodeAngles, binding lines, orientation, area and nodeIDs
         WARNING! Never recalculate a shape that has already been out
@@ -56,22 +103,50 @@ class Shape:
         """
 
         ID = 0
-        for node in self.nodes:
+        for node in self._nodes:
             node.ID = ID
             ID += 1
 
         # Check that calculating stuff is even possible
-        if not self.nodePositionsIsValid():
-            self.valid = False
+        if not self.__nodePositionsIsValid():
+            self._valid = False
             return
 
-        self.findNodeAngles()
-        self.calculateBindingLines()
-        self.calculateOrientation()
-        self.calculateArea()
+        self.__findNodeAngles()
+        self.__calculateBindingLines()
+        self.__calculateOrientation()
+        self.__calculateArea()
 
         # If it isn't simple, it isn't valid
-        self.valid = self.isSimple()
+        self._valid = self.__isSimple()
+
+    def __setNodesRandom(self, xlim=(-0.5, 0.5), ylim=(-0.5, 0.5)):
+        # Decide number of nodes between 4 (incl) and 10 (excl)
+        noOfNodes = np.random.randint(4, 10)
+
+        # Generate two nodes with positive orientation as a start
+        self._nodes = [Node.randomNode(xlim, ylim, 1),
+                      Node.randomNode(xlim, ylim, 1)]
+
+        # Keep track of the tries, don't make too many
+        tries = 0
+        maxTries = 500
+        # Generate nodes
+        while len(self._nodes) < noOfNodes:
+            newNode = Node.randomNode(xlim, ylim)
+            self._nodes.append(newNode)
+            self.__recalculate()
+            if not self.valid:
+                self._nodes.pop()
+
+            tries += 1
+            # If we've tried more than 500 times, start over
+            if tries > maxTries:
+                print(f"We're going deeper (noOfNodes was {noOfNodes})")
+                self.__setNodesRandom(xlim, ylim)
+                return
+
+        return
 
     def mutateInPlace(self, bigMutations=False):
         """
@@ -84,8 +159,8 @@ class Shape:
         """
 
         # Set probabillities
-        mutatePosProb = 1.0/len(self.nodes)
-        mutateRadProb = 1.0/len(self.nodes)
+        mutatePosProb = 1.0/len(self._nodes)
+        mutateRadProb = 1.0/len(self._nodes)
         mutateNoProb = 0.1
         mutateOrProb = 0.1
 
@@ -93,17 +168,17 @@ class Shape:
         posStd = 0.05
         radStd = 0.02
 
-        for node in self.nodes:
+        for node in self._nodes:
             # With probabillity mutatePosProb, mutate position
             if np.random.random_sample() < mutatePosProb:
                 deltaPos = np.random.normal(0, posStd, 2)
                 node.pos += deltaPos
                 # Check that this mutation didn't make it not simple
-                self.recalculate()
+                self.__recalculate()
                 if not self.valid:
                     # If it did, undo it
                     node.pos -= deltaPos
-                    self.recalculate()
+                    self.__recalculate()
 
             # With probabillity mutateRadProb, mutate radius
             if np.random.random_sample() < mutateRadProb:
@@ -113,53 +188,53 @@ class Shape:
                     continue
                 node.r += deltaRad
                 # Check that this mutation didn't make it not simple
-                self.recalculate()
+                self.__recalculate()
                 if not self.valid:
                     # If it did, undo it
                     node.r -= deltaRad
-                    self.recalculate()
+                    self.__recalculate()
 
         if bigMutations:
             # with probabillity mutateNoProb, add a node between two nodes
             if np.random.random_sample() < mutateNoProb:
                 # NodeID for node before which to insert new node
-                nodeID = np.random.choice(len(self.nodes))
-                newNodePos = (self.nodes[nodeID].pos + self.nodes[nodeID-1].pos)/2.0
+                nodeID = np.random.choice(len(self._nodes))
+                newNodePos = (self._nodes[nodeID].pos + self._nodes[nodeID-1].pos)/2.0
                 # r is at least 0.01 and expected around 0.1
                 newNodeR = np.abs(np.random.normal(0.1,0.05)) + 0.01
                 newNodeO = np.random.choice([-1,1])
                 newNode = Node(newNodePos, newNodeR, newNodeO, nodeID)
-                self.nodes.insert(nodeID, newNode)
+                self._nodes.insert(nodeID, newNode)
 
                 # Check that shape is still valid
-                self.recalculate()
+                self.__recalculate()
                 if not self.valid:
-                    del self.nodes[nodeID]
-                    self.recalculate()
+                    del self._nodes[nodeID]
+                    self.__recalculate()
 
             # With probabillity mutateNoProb, remove a node
-            if np.random.random_sample() < mutateNoProb and len(self.nodes) >= 3:
+            if np.random.random_sample() < mutateNoProb and len(self._nodes) >= 3:
                 # NodeID for node to remove
-                nodeID = np.random.choice(len(self.nodes))
-                removedNode = self.nodes[nodeID]
-                del self.nodes[nodeID]
+                nodeID = np.random.choice(len(self._nodes))
+                removedNode = self._nodes[nodeID]
+                del self._nodes[nodeID]
                 # Check that shape is still valid
-                self.recalculate()
+                self.__recalculate()
                 if not self.valid:
-                    self.nodes.insert(nodeID, removedNode)
-                    self.recalculate()
+                    self._nodes.insert(nodeID, removedNode)
+                    self.__recalculate()
 
             # With probabillity mutateOrProb, change a nodes orientation
             if np.random.random_sample() < mutateOrProb:
-                nodeID = np.random.choice(len(self.nodes))
-                self.nodes[nodeID].o *= -1
+                nodeID = np.random.choice(len(self._nodes))
+                self._nodes[nodeID].o *= -1
                 # Check that shape is still valid
-                self.recalculate()
+                self.__recalculate()
                 if not self.valid:
-                    self.nodes[nodeID].o *= -1
-                    self.recalculate()
+                    self._nodes[nodeID].o *= -1
+                    self.__recalculate()
 
-    def nodePositionsIsValid(self):
+    def __nodePositionsIsValid(self):
         """"
         Checks the simple validity of shape
 
@@ -170,9 +245,9 @@ class Shape:
         # No consecutive nodes can be inside each other
         # No consecutive nodes of opposite orientation 
         # can intersect
-        for nodeIndex in range(len(self.nodes)):
-            node1 = self.nodes[nodeIndex]
-            node2 = self.nodes[nodeIndex-1]
+        for nodeIndex in range(len(self._nodes)):
+            node1 = self._nodes[nodeIndex]
+            node2 = self._nodes[nodeIndex-1]
             dist = np.linalg.norm(node1.pos - node2.pos)
             if dist + node1.r < node2.r:
                 return False
@@ -184,46 +259,46 @@ class Shape:
                     return False
         return True
 
-    def isSimple(self):
+    def __isSimple(self):
         "Checks that the shape is simple, if so, return True, else False"
 
         # Check that no two binding lines cross
-        for line1, line2 in itertools.combinations(self.lines, 2):
+        for line1, line2 in itertools.combinations(self._lines, 2):
             if linesIntersect(line1, line2):
                 return False
 
         # Check that no arc intersects a binding line
-        for node in self.nodes:
-            for line in self.lines:
+        for node in self._nodes:
+            for line in self._lines:
                 if segmentIntersectsArc(line, node.pos, node.r,
-                                        self.nodeAngles[node.ID], node.o):
+                                        self._nodeAngles[node.ID], node.o):
                     return False
 
         # Check that no two arcs intersect each other
-        for node1, node2 in itertools.combinations(self.nodes, 2):
+        for node1, node2 in itertools.combinations(self._nodes, 2):
             if arcsIntersect(node1.pos, node1.r,
-                             self.nodeAngles[node1.ID], node1.o,
+                             self._nodeAngles[node1.ID], node1.o,
                              node2.pos, node2.r,
-                             self.nodeAngles[node2.ID], node2.o):
+                             self._nodeAngles[node2.ID], node2.o):
                 return False
 
         return True
 
-    def findNodeAngles(self):
+    def __findNodeAngles(self):
         """
-        Sets self.nodeAngles such that self.nodeAngles[ID] is a tuple
+        Sets self._nodeAngles such that self._nodeAngles[ID] is a np.array
         of the angle from the x axis that the point at which the
         (incomming, outgoing) line touches the node
         """
 
-        N = len(self.nodes)
+        N = len(self._nodes)
 
         # Initialize nodeAngles as an empty dictionary
-        self.nodeAngles = {}
+        self._nodeAngles = {}
 
         for nodeIndex in range(N):
-            node1 = self.nodes[nodeIndex]
-            node2 = self.nodes[(nodeIndex+1) % N]
+            node1 = self._nodes[nodeIndex]
+            node2 = self._nodes[(nodeIndex+1) % N]
 
             # Vector from center of 1 to center of 2
             vector12 = node2.pos - node1.pos
@@ -254,50 +329,48 @@ class Shape:
             alpha = alpha % (2*np.pi)
             beta = beta % (2*np.pi)
 
-            #breakpoint()
             # Update nodeAngles
-            if node1.ID in self.nodeAngles:
-                self.nodeAngles[node1.ID][1] = alpha
+            if node1.ID in self._nodeAngles:
+                self._nodeAngles[node1.ID][1] = alpha
             else:
-                self.nodeAngles[node1.ID] = np.array([None, alpha])
+                self._nodeAngles[node1.ID] = np.array([None, alpha])
 
-            if node2.ID in self.nodeAngles:
-                self.nodeAngles[node2.ID][0] = beta
+            if node2.ID in self._nodeAngles:
+                self._nodeAngles[node2.ID][0] = beta
             else:
-                self.nodeAngles[node2.ID] = np.array([beta, None])
+                self._nodeAngles[node2.ID] = np.array([beta, None])
 
-    def calculateBindingLines(self):
-        N = len(self.nodes)
-        self.lines = np.empty((N,2,2))
-        #breakpoint()
+    def __calculateBindingLines(self):
+        N = len(self._nodes)
+        self._lines = np.empty((N,2,2))
         for nodeIndex in range(N):
-            node1 = self.nodes[nodeIndex]
-            node2 = self.nodes[(nodeIndex+1) % N]
+            node1 = self._nodes[nodeIndex]
+            node2 = self._nodes[(nodeIndex+1) % N]
 
-            alpha = self.nodeAngles[node1.ID][1]
-            beta = self.nodeAngles[node2.ID][0]
+            alpha = self._nodeAngles[node1.ID][1]
+            beta = self._nodeAngles[node2.ID][0]
 
             linePoint1 = node1.pos + node1.r * \
                 np.array([np.cos(alpha), np.sin(alpha)])
             linePoint2 = node2.pos + node2.r * \
                 np.array([np.cos(beta), np.sin(beta)])
 
-            self.lines[nodeIndex] = np.array([linePoint1, linePoint2])
+            self._lines[nodeIndex] = np.array([linePoint1, linePoint2])
 
-    def calculateOrientation(self):
+    def __calculateOrientation(self):
         """
-        Set self.positive orientation to true if the shape is
+        Set self._o to 1 if the shape is
         positively oriented, that is, if the total angle change
-        by the nodes is positive.
+        by the nodes is positive, and -1 otherwise.
         If the shape is positively oriented, ALL nodes with positive
         orientation are inside the shape and ALL nodes with negative
         orientation are outside.
         """
 
         cumulativeAngleChange = 0
-        for node in self.nodes:
+        for node in self._nodes:
             # Calculate the angle change due to node
-            angleIn, angleOut = self.nodeAngles[node.ID]
+            angleIn, angleOut = self._nodeAngles[node.ID]
             # Angle change is out - in if positively oriented,
             # and in - out if negatively oriented
             angleChange = (node.o*(angleOut - angleIn)) % (2 * np.pi)
@@ -306,9 +379,9 @@ class Shape:
             elif node.o == -1:
                 cumulativeAngleChange -= angleChange
 
-        self.o = 1 if cumulativeAngleChange > 0 else -1
+        self._o = 1 if cumulativeAngleChange > 0 else -1
 
-    def calculateArea(self):
+    def __calculateArea(self):
         """
         Calculates the area of the shape
         step one: calculate the area of the shape created by
@@ -321,9 +394,9 @@ class Shape:
         # Also sum up the circle sector areas
         points = np.empty((0, 2), float)
         circleSectorAreas = 0
-        for node in self.nodes:
+        for node in self._nodes:
             pos = node.pos
-            angleIn, angleOut = self.nodeAngles[node.ID]
+            angleIn, angleOut = self._nodeAngles[node.ID]
             deltap1 = node.r * np.array([np.cos(angleIn), np.sin(angleIn)])
             deltap2 = node.r * np.array([np.cos(angleOut), np.sin(angleOut)])
 
@@ -337,7 +410,7 @@ class Shape:
             deltaAngle = (node.o * (angleOut - angleIn)) % (2*np.pi)
             # Circle sector should be added if inside and subtracted if
             # outside
-            circleSectorAreas += self.o * node.o * node.r**2 * deltaAngle / 2
+            circleSectorAreas += self._o * node.o * node.r**2 * deltaAngle / 2
 
         # Shift points so that first point is at 0,0
         points = points - points[0]
@@ -352,5 +425,5 @@ class Shape:
         area = abs(area) / 2.0
 
         # Add the circleSectorAreas
-        self.area = area + circleSectorAreas
+        self._area = area + circleSectorAreas
 
